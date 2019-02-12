3b84a08239053f0c252af0355c0dc024799fe180
Denílson dos Santos Ebling <d.ebling8@gmail.com>
2019-02-12 20:51:34 -0200

    Changed solarized colors to the base16 version

--- config.h
+++ config.h
@@ -82,25 +82,23 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
-/* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* solarized light */
-	"#eee8d5",  /*  0: black    */
-	"#dc322f",  /*  1: red      */
-	"#859900",  /*  2: green    */
-	"#b58900",  /*  3: yellow   */
-	"#268bd2",  /*  4: blue     */
-	"#d33682",  /*  5: magenta  */
-	"#2aa198",  /*  6: cyan     */
-	"#073642",  /*  7: white    */
-	"#fdf6e3",  /*  8: brblack  */
-	"#cb4b16",  /*  9: brred    */
-	"#93a1a1",  /* 10: brgreen  */
-	"#839496",  /* 11: bryellow */
-	"#657b83",  /* 12: brblue   */
-	"#6c71c4",  /* 13: brmagenta*/
-	"#586e75",  /* 14: brcyan   */
-	"#002b36",  /* 15: brwhite  */
+     "#fdf6e3", /* base00 */
+     "#dc322f", /* base08 */
+     "#859900", /* base0B */
+     "#b58900", /* base0A */
+     "#268bd2", /* base0D */
+     "#6c71c4", /* base0E */
+     "#2aa198", /* base0C */
+     "#586e75", /* base05 */
+     "#839496", /* base03 */
+     "#cb4b16", /* base09 */
+     "#eee8d5", /* base01 */
+     "#93a1a1", /* base02 */
+     "#657b83", /* base04 */
+     "#073642", /* base06 */
+     "#d33682", /* base0F */
+     "#002b36", /* base07 */
 };
 
 
@@ -108,10 +106,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 12;
-unsigned int defaultbg = 8;
-static unsigned int defaultcs = 14;
-static unsigned int defaultrcs = 15;
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 13;
+static unsigned int defaultrcs = 0;
 
 /*
  * Default shape of cursor
